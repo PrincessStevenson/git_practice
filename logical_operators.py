f8da92ea2aef5b42d604543215ca2e152c355039
user <user@users-mbp.home>
2021-06-15 12:24:07 +0100

    initial setup

---
+++ logical_operators.py
@@ -0,0 +1,9 @@
+zsol_hungry = True
+zsol_tired = True
+
+if zsol_hungry and zsol_tired:
+  print("Zsol is hangry!!!")
+
+zsol_tired = False
+if zsol_hungry or zsol_tired:
+  print("Zsol is grumpy!")
---
+++ what_animal.py
@@ -0,0 +1,21 @@
+
+# if animal is "chicken"
+# then print "This is my favourite animal too!!"
+# else
+# if animal is turtle
+# then print "I love turtles!"
+# print "I am sad"
+# end
+
+animal = input("What is favourite animal?")
+
+if animal == "chicken":
+  print("This is my favourite animal too!!")
+elif animal == "turtle":
+  print("I love turtles!")
+else:
+  print("I am sad!")
+
+
+
+
